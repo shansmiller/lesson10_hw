acc4d74f8ea00d46316557c2bfdf0f4356e23b82
ShannonMiller <shans.miller@gmail.com>
2015-08-27 18:34:35 -0400

    added java to merch page. expeperimented with java with beers page.

--- js/java.js
+++ js/java.js
@@ -11,14 +11,46 @@ $(document).ready(function(){
 
 		alert("Please come back when you're 21!")
 	
-	});
 
-	 $(".circle").hover(function(){
+});
+
+	 	$(".circle").hover(function(){
 		  $(this).css("width","70px")
 		  
 		 },function(){
 		 	$(this).css("width","60px")
-		 })
+		 });
+	
+	  	$("#item").hover(function(){
+	  		$("#price").css("visibility", "visible");
+	 		$(this).faddClass("hidden");
 
-});
+
+	 	 });
+	  	$("#itemB").hover(function(){
+	  		$("#priceB").css("visibility", "visible");
+	 		$(this).addClass("hidden");
+
+
+	 	 });
+
+	  	$("#itemC").hover(function(){
+	  		$("#priceC").css("visibility", "visible");
+	 		$(this).addClass("hidden");
+
+	 	 });
+	  		$("#itemD").hover(function(){
+	  		$("#priceD").css("visibility", "visible");
+	 		$(this).addClass("hidden");
+
+	 	 });
+
+
+	  	$(".beerCircle").hover(function(){
+	  		$(this).css("background-color", "black");
+	 		$(this).css("background-color","#329406");
+	 	});
+
+
+}); //this should contain everything
 
